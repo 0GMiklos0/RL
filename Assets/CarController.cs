84f3de479f91632ddb4e55009bd4d2450377d7a6
0GMiklos0 <90982702+0GMiklos0@users.noreply.github.com>
2023-11-23 09:28:34 +0100

    Add files via upload

---
+++ Assets/CarController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class CarController : Agent
+{
+    public int CollectedCheckpoint;
+    [SerializeField] private Transform targetTransform;
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(-24.15858f, 0.03f, 8.39f);
+        CollectedCheckpoint = 0;
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        Debug.Log(actions.DiscreteActions[0]);
+    }
+    private void OnTriggerEvent(Collision collision)
+    {
+        if (collision.collider.tag == "CheckPoint") { 
+            AddReward(1f);
+            CollectedCheckpoint++;
+        }
+        if (collision.collider.tag == "Wall") { 
+            AddReward(-1f);
+            EndEpisode();
+        }
+        if (collision.collider.tag == "FinishLine" && CollectedCheckpoint == 3) {
+            SetReward(4f);
+            EndEpisode();
+        }
+    }
+    public void OnActionReceived()
+    {
+        float move = ;
+    }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> actions = actionsOut.ContinuousActions;
+        GetInputs();
+        AnimateWheels();
+        Move(); Steer(); Brake();
+    }
+    //The car controller
+
+    public enum Axel
+    {
+        Front,
+        Rear
+    }
+
+    [Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
+
+    public float maxAcceleration = 30.0f;
+    public float brakeAcceleration = 50.0f;
+
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public Vector3 _centerOfMass;
+
+    public List<Wheel> wheels;
+
+    float moveInput;
+    float steerInput;
+
+    private Rigidbody carRb;
+
+    void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = _centerOfMass;
+    }
+
+    public void MoveInput(float input)
+    {
+        moveInput = input;
+    }
+
+    public void SteerInput(float input)
+    {
+        steerInput = input;
+    }
+
+    void GetInputs()
+    {
+        
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+        foreach(var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
+        }
+    }
+
+    void Steer()
+    {
+        foreach(var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+    }
+
+    void Brake()
+    {
+        if (Input.GetKey(KeyCode.Space) || moveInput == 0)
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration * Time.deltaTime;
+            }
+        }
+        else
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 0;
+            }
+        }
+    }
+
+    void AnimateWheels()
+    {
+        foreach(var wheel in wheels)
+        {
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
+        }
+    }
+}
---
+++ Assets/cameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public float moveSmoothness;
+    public float rotSmoothness;
+    public Vector3 moveOffset;
+    public Vector3 rotOffset;
+    public Transform carTarget;
+    void HandleMovement()
+    {
+        Vector3 targetPos = new Vector3();
+        targetPos = carTarget.TransformPoint(moveOffset);
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSmoothness * Time.deltaTime);
+    }
+    void FollowTarget()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+    void FixedUpdate()
+    {
+        FollowTarget();
+    }
+    void HandleRotation()
+    {
+        var direction = carTarget.position - transform.position;
+        var rotation = new Quaternion();
+        rotation = Quaternion.LookRotation(direction + rotOffset, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSmoothness * Time.deltaTime);
+    }
+}
